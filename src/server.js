65c0fb3f3b6b83c0c46f3837cb5384ce4c749ca4
Nadeem, Mohammad <mnadeem@optum.com>
2021-02-16 00:33:39 +0530

    more changes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
+import opentelemetry from '@opentelemetry/api'
 import express from 'express';
 
 const PORT = process.env.PORT || 5555;
+
+const tracer = opentelemetry.trace.getTracer('nodejs-opentelemetry-tempo');
 
 const app = express();
 app.use(express.json());
  
 app.get('/health', (req, res) => {
-    console.log('Calling res.send');
+    const span = tracer.startSpan('op');
+    const ctx = span.context();
+    span.setAttribute('key', 'value');
+    
+    console.log('TraceId is : ' + ctx.traceId);
+    span.end();
     return res.status(200).send({message: "Health is good"});
 });
  
--- src/tracing.js
+++ src/tracing.js
@@ -1,22 +1,37 @@
-import opentelemetry from '@opentelemetry/api'
 import {LogLevel} from '@opentelemetry/core'
 import {NodeTracerProvider} from '@opentelemetry/node'
 import {registerInstrumentations} from '@opentelemetry/instrumentation'
 import {JaegerExporter} from '@opentelemetry/exporter-jaeger'
 import {SimpleSpanProcessor, BatchSpanProcessor, ConsoleSpanExporter} from '@opentelemetry/tracing'
 
-//opentelemetry.diag.setLogger(new opentelemetry.DiagConsoleLogger());
-//opentelemetry.diag.setLogLevel(opentelemetry.DiagLogLevel.DEBUG);
-
 const provider = new NodeTracerProvider ({
-    logLevel: LogLevel.DEBUG
+    plugins: {
+        express: {
+          enabled: true,
+          path: '@opentelemetry/plugin-express',
+        }
+    },
+    logLevel: LogLevel.ERROR      
 });
 
 registerInstrumentations({
     tracerProvider: provider
 });
 
+/**
+ * Registering the provider with the API allows it to be discovered
+ * and used by instrumentation libraries. The OpenTelemetry API provides
+ * methods to set global SDK implementations, but the default SDK provides
+ * a convenience method named `register` which registers same defaults
+ * for you.
+ *
+ * By default the NodeTracerProvider uses Trace Context for propagation
+ * and AsyncHooksScopeManager for context management. To learn about
+ * customizing this behavior, see API Registration Options below.
+ */
+provider.register();
 
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
 const options = {
     serviceName: 'nodejs-opentelemetry-tempo',
@@ -29,12 +44,12 @@ const options = {
     maxPacketSize: 65000 // optional
 }
 
-const exporter = new JaegerExporter(options);
-const consoleExporter = new ConsoleSpanExporter();
-
-provider.addSpanProcessor(new SimpleSpanProcessor(consoleExporter));
-provider.addSpanProcessor(new BatchSpanProcessor(exporter));
-provider.register();
+/**
+ * The SimpleSpanProcessor does no batching and exports spans
+ * immediately when they end. For most production use cases,
+ * OpenTelemetry recommends use of the BatchSpanProcessor.
+ */
+provider.addSpanProcessor(new BatchSpanProcessor(new JaegerExporter(options)));
 
 console.log("tracing initialized");
 
