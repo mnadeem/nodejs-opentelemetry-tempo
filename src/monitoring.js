63e82948ba4c2717e793bf4929b51f9c14be7f99
Nadeem, Mohammad <mnadeem@optum.com>
2021-02-16 14:06:24 +0530

    prom-client for metrics

---
+++ src/monitoring.js
@@ -0,0 +1,26 @@
+import promClient from 'prom-client';
+
+// Create a Registry which registers the metrics
+export const register = new promClient.Registry()
+promClient.collectDefaultMetrics({ register });
+
+const Histogram = promClient.Histogram;
+const requestDuration = new Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'request duration histogram',
+    labelNames: ['handler' , 'method', 'statuscode'],
+    buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10],
+});
+
+// Register the histogram
+register.registerMetric(requestDuration)
+
+export const measureRequestDuration = (req, res, next) => {
+    const start = Date.now();
+   res.once('finish', () => {
+    const duration = Date.now() - start;
+    requestDuration.labels(req.url, req.method, res.statusCode).observe(duration)
+   });
+ 
+  next();
+};
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-import {tracer} from './tracing';
+import { tracer } from './tracing';
+import { register, measureRequestDuration } from './monitoring'
 
 import opentelemetry from '@opentelemetry/api'
 import express from 'express';
@@ -7,17 +8,29 @@ const PORT = process.env.PORT || 5555;
 
 const app = express();
 app.use(express.json());
- 
+
+app.use(measureRequestDuration);
+
+// Setup server to Prometheus scrapes:
+app.get('/metrics', async (req, res) => {
+    try {
+        res.set('Content-Type', register.contentType);
+        res.end(await register.metrics());
+    } catch (ex) {
+        res.status(500).end(ex);
+    }
+});
+
 app.get('/health', (req, res) => {
     //const span = tracer.startSpan('op');
     //const ctx = span.context();
     //span.setAttribute('key', 'value');
-    
+
     //console.log('TraceId is : ' + ctx.traceId);
     //span.end();
-    return res.status(200).send({message: "Health is good"});
+    return res.status(200).send({ message: "Health is good" });
 });
- 
+
 app.listen(PORT, () => {
     console.log('App is listening for requests on port %d', PORT);
 });
