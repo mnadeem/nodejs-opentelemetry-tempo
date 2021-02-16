dad31c19f23de82ef9ea63895550bf8bab6c3d1e
Nadeem, Mohammad <mnadeem@optum.com>
2021-02-16 18:54:50 +0530

    more changes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import {addCorsHeaders} from './cors'
+import {addCorsHeaders } from './cors'
 import { tracer } from './tracing';
-import { measureRequestDuration, registerPromMetrics} from './monitoring';
-import { context, getSpan, getSpanContext} from '@opentelemetry/api';
+import { measureRequestDuration, registerPromMetrics } from './monitoring';
+import { context, getSpan, getSpanContext } from '@opentelemetry/api';
 
 import cors from 'cors'
 import opentelemetry from '@opentelemetry/api'
@@ -19,16 +19,36 @@ app.use(measureRequestDuration);
 app.get('/metrics', registerPromMetrics);
 
 app.get('/health', (req, res) => {
-    
-    //const span = getSpan(opentelemetry.context.active());
-    //console.log(span.context().traceId);
-
     const spanContext = getSpanContext(opentelemetry.context.active());
 
     console.log("TraceId : " + spanContext.traceId);
+    const parentSpan = getSpan(opentelemetry.context.active());
+    doSomeWorkInNewSpan(parentSpan);
 
     return res.status(200).send({ message: "Health is good" });
 });
+
+const doSomeWorkInNewSpan = (parentSpan) => {
+    
+    const childSpan = tracer.startSpan('doSomeWorkInNewSpan', {
+        parentSpan, attributes: { 'code.function' : 'doSomeWorkInNewSpan' }
+    });
+    childSpan.setAttribute('code.filepath', "test");
+    doSomeWorkInNewNestedSpan(childSpan);
+    childSpan.end();
+}
+
+const doSomeWorkInNewNestedSpan = (parentSpan) => {
+    const childSpan = tracer.startSpan('doSomeWorkInNewNestedSpan', {
+        parentSpan, attributes: { 'code.function' : 'doSomeWorkInNewNestedSpan' }
+    });
+    childSpan.setAttribute('code.filepath', "test");
+    //Do some work
+    for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {
+        // empty
+    }
+    childSpan.end();
+}
 
 app.listen(PORT, () => {
     console.log('App is listening for requests on port %d', PORT);
